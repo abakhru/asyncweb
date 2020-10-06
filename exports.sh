7c820e18e8368c21e2f39ea5b8caeccd4c94fc8d
Amit Bakhru <bakhru@me.com>
2020-10-05 19:56:26 -0700

    fix pytest.ini file

--- exports.sh
+++ exports.sh
@@ -6,7 +6,6 @@ export POSTGRES_DB="asyncweb"
 export POSTGRES_HOST="db"
 #export POSTGRES_HOST=${POSTGRES_HOST:-"0.0.0.0"}
 export DATABASE_URL="postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}/${POSTGRES_DB}"
-export MONGO_DETAILS="mongodb://mongourl:port"
 export WORKSPACE=${WORKSPACE:-$PWD}
 export PYTHONPATH=${WORKSPACE}
 export VIRTUAL_ENV=${VIRTUAL_ENV:-WORKSPACE/env}
