b684341244bfa3ff1daf49843b4d29569b79dd9b
Amit Bakhru <bakhru@me.com>
2020-10-21 15:00:48 -0700

    slim down docker image size

--- bin/quick_start.sh
+++ bin/quick_start.sh
@@ -11,5 +11,5 @@ cd "${WORKSPACE}"
 source bin/exports.sh
 
 
-pip3 install -U pip setuptools wheel
-pip3 install -e .
+pip3 install --no-cache-dir -U pip setuptools wheel
+pip3 install --no-cache-dir -e .
