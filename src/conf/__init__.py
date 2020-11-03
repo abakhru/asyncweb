524b5200cfdb5be4f7aed803f17e59395ed2f3e1
Amit Bakhru <bakhru@me.com>
2020-11-03 13:35:19 -0800

    Remove secrets

--- src/conf/__init__.py
+++ src/conf/__init__.py
@@ -1,4 +1,7 @@
 import json
 from pathlib import Path
 
+from src import APP_HOME
+
 config = json.load(Path(__file__).parent.joinpath('conf.json').open())
+config['project']['secret'] = APP_HOME.joinpath('.secret').read_text().strip()
